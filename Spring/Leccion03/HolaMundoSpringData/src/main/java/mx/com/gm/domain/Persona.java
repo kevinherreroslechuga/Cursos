a3d0edf2d1083188423b991f173306ca87bee88d
kevinherreroslechuga <kev.herreros@gmail.com>
2024-08-20 21:33:59 -0600

    evitar subir

--- Spring/Leccion03/HolaMundoSpringData/src/main/java/mx/com/gm/domain/Persona.java
+++ Spring/Leccion03/HolaMundoSpringData/src/main/java/mx/com/gm/domain/Persona.java
@@ -3,9 +3,7 @@ package mx.com.gm.domain;
 import lombok.Data;
 
 @Data//esta anotacion es parte de loombok no de spring, es opcional utilizarla, es para crear todos los metodos de getters y setter para configurar la clase como un java bean
+@Entity
 public class Persona {
-    private String nombre;
-    private String apellido;
-    private String email;
-    private String telefono;
+    
 }
