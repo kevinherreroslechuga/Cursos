cbf4a42fc5de97150b42f065ab4a681fc26c0b18
Kevin Herreros <koherreros@bancoppel.com>
2024-08-28 18:54:02 -0600

    add micros

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
@@ -18,6 +19,11 @@ public class ProductoController {
 	@GetMapping("/listar")
 	public List<Producto>Listar(){//siempre publicos 
 		return productoService.findAll();
+	}
+	
+	@GetMapping("/ver/{id}")
+	public Producto detalle(@PathVariable Long id) {
+		return productoService.findById(id);
 	}
 
 }
---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony', 700, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Apple', 1000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Notebook', 1000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Hewlett Packard', 200, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Bianchi', 600, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Nike', 100, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Adidas', 200, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Reebok', 300, NOW());
+-- hibernate busca el archivo import si es que existe y como la base h2 en memoria se genera junto con las tablas 
