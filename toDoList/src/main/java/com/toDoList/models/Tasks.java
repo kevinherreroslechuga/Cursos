2b29ccb5f907fa9ac366548379e4ae24a56093af
Kevin Herreros <koherreros@bancoppel.com>
2024-09-09 19:02:57 -0600

    todo list

---
+++ toDoList/src/main/java/com/toDoList/models/Tasks.java
@@ -0,0 +1,13 @@
+package com.toDoList.models;
+
+public class Tasks {
+	
+	
+	public String title;
+	public String description;
+	public Date creatAt;
+	public Date finishAt;
+	public boolean finishIt;
+	public String stateOf;
+
+}
